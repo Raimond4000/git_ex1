33e44da877bda12a6b440a9ce65a4192df137029
Raimond Virkus <raimond.virkus@mail.ee>
2018-10-09 22:16:30 +0300

    Made scanner + sum calculator

--- src/com/git_ex1/app/Sum.java
+++ src/com/git_ex1/app/Sum.java
@@ -1,8 +1,25 @@
 package com.git_ex1.app;
 
+import java.util.Scanner;
+
 public class Sum {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int[] allNumbers = new int[5];
+
+        int inputNumber;
+        for (inputNumber = 0; inputNumber < allNumbers.length; ++inputNumber) {
+            System.out.println("Please enter nr");
+            allNumbers[inputNumber] = scanner.nextInt();
+
+        }
+        int sum = 0;
+        for (inputNumber = 0; inputNumber < allNumbers.length; ++inputNumber) {
+            sum += allNumbers[inputNumber];
+        }
+        System.out.println(sum);
     }
+
 }
