39b0c2ba2b762deb89f6119703f85cd8aaef891b
Raimond Virkus <raimond.virkus@mail.ee>
2018-10-09 22:33:33 +0300

    Users gives array size

--- src/com/git_ex1/app/Sum.java
+++ src/com/git_ex1/app/Sum.java
@@ -7,7 +7,15 @@ public class Sum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[] allNumbers = new int[5];
+        System.out.println("Please enter nr for array size");
+
+        int amountOfElementsToScan = scanner.nextInt();
+        if (amountOfElementsToScan <= 0) {
+            System.out.println("Please enter nr that is > 0");
+            return;
+        }
+
+        int[] allNumbers = new int[amountOfElementsToScan];
 
         int inputNumber;
         for (inputNumber = 0; inputNumber < allNumbers.length; ++inputNumber) {
@@ -20,6 +28,8 @@ public class Sum {
             sum += allNumbers[inputNumber];
         }
         System.out.println(sum);
+
+
     }
 
 }
